63314fe3c404dc5131cebd85e834a556cddd6628
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-01-12 10:02:13 +0900

    [new] クライアントサイドのクラスを作成

---
+++ src/MyClient.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * @author : akira.shinohara
+ * @since : 2018/01/12
+ */
+public class MyClient {
+    public static void main(String[] args) {
+        MyClientSocket mcs = new MyClientSocket();
+    }
+}
+
+class MyClientSocket {
+    Socket socket;
+    
+    MyClientSocket() {
+        try {
+            Socket socket = new Socket(InetAddress.getByName("127.0.0.0"), 1234);
+        } catch (IOException err) {
+            System.out.println(err.toString());
+        }
+    }
+}
